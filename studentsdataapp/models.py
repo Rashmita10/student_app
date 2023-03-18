f13c1d47e0b8ca46504ebc813d400b7468ba1d2c
Rashmita10 <120892726+Rashmita10@users.noreply.github.com>
2023-03-18 13:14:01 +0530

    first commit

---
+++ studentsdataapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+class StudentsData(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=50)
+    mobile=models.BigIntegerField()
+    course=models.CharField(max_length=50)
+    fee=models.IntegerField()
+    dob=models.DateField()
+    location=models.CharField(max_length=50)
+
+# Create your models here.
